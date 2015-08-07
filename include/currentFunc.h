2ddc8e2835fe818f0260d95e333a1def8aed0762
heliophile <heliophile@onkars-MBP.home>
2015-08-07 00:23:09 -0400

    Print name of function body being executed

---
+++ include/currentFunc.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#if __STDC_VERSION__ < 199901L
+#if __GNUC__ >= 2
+#define __func__ __FUNCTION__
+#else
+#define __func__ "<unknown>"
+#endif
+#endif
+
+#define __currentfunc__ __func__
